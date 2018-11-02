d134ae24e41e66f85b29ac2c05bf056f0a84f54c
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2018-11-02 19:14:02 +0300

    updated Fibonacci

--- main/java/kyu3/Fibonacci.java
+++ main/java/kyu3/Fibonacci.java
@@ -3,23 +3,44 @@ package kyu3;
 import java.math.BigInteger;
 
 //  Task description on https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
-//  Time complexity is O(n)
+//  Time complexity is O(log n)
 //  Space complexity is O(1)
 public class Fibonacci {
     public static BigInteger fib(BigInteger n) {
         int count = Math.abs(n.intValue()); // because |n| < 2^32 - 1
-        boolean isSameSign = (n.signum() == 1) ? true : (count % 2 == 1) ? true : false;
-        BigInteger now = BigInteger.ZERO;
-        BigInteger next = BigInteger.ONE;
-        BigInteger temp;
+        boolean isPositive = (n.signum() != -1) || (count % 2 == 1);
+        BigInteger[][] a = {{BigInteger.ONE, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ZERO}};
+        BigInteger[][] res = new BigInteger[1][];
+
+        if (n.signum() == 0)
+            return BigInteger.ZERO;
 
         while (count > 0) {
-            temp = next;
-            next = next.add(now);
-            now = temp;
-            count--;
+            if (count % 2 == 1)
+                res = (res[0] == null) ? a.clone() : multiplyMatrix(res, a);
+            if (count != 1)  // skip last multiplication
+                a = multiplyMatrix(a, a);
+            count >>= 1;
         }
 
-        return (isSameSign) ? now : now.negate();
+        return (isPositive) ? res[0][1] : res[0][1].negate();
+    }
+
+    public static BigInteger[][] multiplyMatrix(BigInteger[][] A, BigInteger[][] B) {
+        int aRows = A.length;
+        int bRows = B.length;
+        int aColumns = bRows; // = A[0].length;
+        int bColumns = B[0].length;
+        BigInteger[][] res = new BigInteger[aRows][bColumns];
+
+        for (int i = 0; i < aRows; i++) {
+            for (int j = 0; j < bColumns; j++) {
+                for (int k = 0; k < bRows; k++) {
+                    res[i][j] = (res[i][j] == null) ? A[i][k].multiply(B[k][j]) : res[i][j].add(A[i][k].multiply(B[k][j]));
+                }
+            }
+        }
+
+        return res;
     }
 }
