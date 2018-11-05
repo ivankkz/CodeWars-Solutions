dc85d4d88844e04ba6be805b627a15346263854e
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2018-11-05 12:30:55 +0300

    updated FindOdd and Fibonacci

--- main/java/kyu3/Fibonacci.java
+++ main/java/kyu3/Fibonacci.java
@@ -7,29 +7,28 @@ import java.math.BigInteger;
 //  Space complexity is O(1)
 public class Fibonacci {
     public static BigInteger fib(BigInteger n) {
-        int count = Math.abs(n.intValue()); // because |n| < 2^32 - 1
-        boolean isPositive = (n.signum() != -1) || (count % 2 == 1);
+        int count = Math.abs(n.intValue()) - 1; // because |n| < 2^32 - 1
+        boolean isPositive = (n.signum() != -1) || (count % 2 == 0);
         BigInteger[][] a = {{BigInteger.ONE, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ZERO}};
-        BigInteger[][] res = new BigInteger[1][];
+        BigInteger[][] res = {{BigInteger.ONE, BigInteger.ZERO}, {BigInteger.ZERO, BigInteger.ONE}};
 
         if (n.signum() == 0)
             return BigInteger.ZERO;
 
         while (count > 0) {
             if (count % 2 == 1)
-                res = (res[0] == null) ? a.clone() : multiplyMatrix(res, a);
+                res = multiplyMatrix(res, a);
             if (count != 1)  // skip last multiplication
                 a = multiplyMatrix(a, a);
             count >>= 1;
         }
 
-        return (isPositive) ? res[0][1] : res[0][1].negate();
+        return (isPositive) ? res[0][0] : res[0][0].negate();
     }
 
     public static BigInteger[][] multiplyMatrix(BigInteger[][] A, BigInteger[][] B) {
         int aRows = A.length;
         int bRows = B.length;
-        int aColumns = bRows; // = A[0].length;
         int bColumns = B[0].length;
         BigInteger[][] res = new BigInteger[aRows][bColumns];
 
--- main/java/kyu6/FindOdd.java
+++ main/java/kyu6/FindOdd.java
@@ -1,25 +1,19 @@
 package kyu6;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 //  Task description on https://www.codewars.com/kata/54da5a58ea159efa38000836
 public class FindOdd {
     public static int findIt(int[] a) {
-        Map<Integer, Integer> res = new HashMap<>();
-        Integer[] aInt = Arrays.stream(a).boxed().toArray(Integer[]::new);
+        int i = 0;
 
-        for (int i = 0; i < aInt.length; i++) {
-            if (res.containsKey(aInt[i]))
-                res.put(aInt[i], res.get(aInt[i]) + 1);
-            else
-                res.put(aInt[i], 1);
-        }
+        Arrays.sort(a);
+        while (i < a.length) {
+            do {
+                i++;
+            } while (i < a.length && a[i - 1] == a[i]);
 
-        for (Map.Entry<Integer, Integer> elem : res.entrySet()) {
-            if (elem.getValue() % 2 == 1)
-                return elem.getKey();
+            if (i % 2 == 1) return a[i - 1];
         }
 
         return 0;
